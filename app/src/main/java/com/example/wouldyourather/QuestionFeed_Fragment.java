806d64f380d154cd70a89d00bc98a4c7a057e954
luca <LucaSchmidLS1@gmail.com>
2020-08-05 18:21:26 +0200

    viewpager connected with tabLayout (working) - zeigt über den pages die kategorien an

--- app/src/main/java/com/example/wouldyourather/QuestionFeed_Fragment.java
+++ app/src/main/java/com/example/wouldyourather/QuestionFeed_Fragment.java
@@ -1,27 +1,25 @@
 package com.example.wouldyourather;
 
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
-
-import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 
 public class QuestionFeed_Fragment extends Fragment {
 
     private static final int NUM_PAGES = 5;
     private ViewPager2 viewPager;
     private FragmentStateAdapter pagerAdapter;
+    private TabLayout tabLayout;
 
     public QuestionFeed_Fragment() {
         // Required empty public constructor
@@ -35,6 +33,15 @@ public class QuestionFeed_Fragment extends Fragment {
         viewPager = v.findViewById(R.id.pager);
         pagerAdapter = new QuestionFeed_Fragment.ScreenSlidePagerAdapter(this);
         viewPager.setAdapter(pagerAdapter);
+
+        //tab layout connection
+        tabLayout = v.findViewById(R.id.tabLayout);
+        new TabLayoutMediator(tabLayout, viewPager,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        tab.setText("Kategorie " + (position + 1));
+                    }
+                }).attach();
 
         return v;
     }
