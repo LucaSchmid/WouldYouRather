d46176a222bd8bba0da134e6d0d9784050a6ea2a
luca <LucaSchmidLS1@gmail.com>
2020-08-06 14:14:45 +0200

    spinner for choosing a kategory is working

--- app/src/main/java/com/example/wouldyourather/newQuestion_Fragment.java
+++ app/src/main/java/com/example/wouldyourather/newQuestion_Fragment.java
@@ -3,6 +3,7 @@ package com.example.wouldyourather;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,34 +11,51 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import static androidx.core.content.ContextCompat.getSystemService;
 import static androidx.core.content.ContextCompat.getSystemServiceName;
 
-public class newQuestion_Fragment extends Fragment implements View.OnClickListener {
+public class newQuestion_Fragment extends Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     public newQuestion_Fragment() {
         // Required empty public constructor
     }
-    private Button Button_AddQuestion, Button_Refresh;
+    private Button Button_AddQuestion;
     TextView textViewCheckConnection;
     View view;
+    EditText WouldYouRather1, WouldYouRather2;
+    String wouldYouRather1, wouldYouRather2, Kategory;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_new_question_, container, false);
+        //
+        WouldYouRather1 = (EditText) view.findViewById(R.id.EditText1);
+        WouldYouRather2 = (EditText) view.findViewById(R.id.EditText2);
+        //
+
         Button_AddQuestion = (Button) view.findViewById(R.id.ButtonAddQuestion);
-        Button_Refresh = (Button) view.findViewById(R.id.ButtonRefresh);
         Button_AddQuestion.setOnClickListener(this);
-        Button_Refresh.setOnClickListener(this);
         textViewCheckConnection = view.findViewById(R.id.textViewCheckConnection);
 
-        checkConnection();
+        final Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        String [] values =
+                {"Kategorie 1","Kategorie 2","Kategorie 3","Kategorie 4","Kategorie 5"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
 
+        spinner.setOnItemSelectedListener(this);
+
+        checkConnection();
         return view;
     }
 
@@ -46,14 +64,8 @@ public class newQuestion_Fragment extends Fragment implements View.OnClickListen
     public void onClick(View view) {
 
         switch (view.getId()){
-            case R.id.ButtonRefresh:
-                checkConnection();
-                break;
             case R.id.ButtonAddQuestion:
-                if (checkConnection()) {
-                    Toast.makeText(getActivity(),
-                            "added to the DB", Toast.LENGTH_LONG).show();
-                }
+                addQuestion(view);
                 break;
         }
 
@@ -63,16 +75,54 @@ public class newQuestion_Fragment extends Fragment implements View.OnClickListen
         ConnectivityManager connectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         if(networkInfo!=null && networkInfo.isConnected()){
-            Button_AddQuestion.setEnabled(true);
             textViewCheckConnection.setVisibility(View.INVISIBLE);
-            Button_Refresh.setVisibility(View.INVISIBLE);
             return true;
         }
         else {
-            Button_AddQuestion.setEnabled(false);
             textViewCheckConnection.setVisibility(View.VISIBLE);
-            Button_Refresh.setVisibility(View.VISIBLE);
             return false;
         }
+    }
+
+    public void addQuestion(View view){
+        /*if (checkConnection()) {
+
+            wouldYouRather1 = WouldYouRather1.getText().toString();
+            wouldYouRather2 = WouldYouRather2.getText().toString();
+            Toast.makeText(getActivity(),
+                    "added to the DB", Toast.LENGTH_LONG).show();
+        }*/
+    }
+    /*class BackgroudTask extends AsyncTask<Void,Void,Void>
+    {
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+    }*/
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Kategory = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
